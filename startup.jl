ac979321f516dab44547f810aed4b62a93560897
Donglai Gong <gong@vims.edu>
2020-01-16 10:13:49 -0500

    moved npp_path_setup to NPP subdirectory, updated startup.jl

--- startup.jl
+++ startup.jl
@@ -1,5 +1,17 @@
-#ENV["JULIA_PARDISO"] = "/Users/gong/lib/Pardiso/libpardiso600-MACOS-X86-64.dylib"
+ENV["JULIA_PARDISO"] = "/Users/gong/lib/Pardiso/libpardiso600-MACOS-X86-64.dylib"
 ENV["PYTHON"] = string(ENV["HOME"], "/anaconda3/bin/python3")
+#ENV["PYTHON"] = string(ENV["HOME"], "/.julia/conda/3/bin/python3")
+#ENV["CONDA_JL_HOME"] =
+
+ocean_julia = ENV["HOME"] * "/GitHub/ocean_julia"
+if (ocean_julia in LOAD_PATH) == false
+    push!(LOAD_PATH, ocean_julia);
+end
+
+ocean_julia_npp = ENV["HOME"] * "/GitHub/ocean_julia/NPP"
+if (ocean_julia_npp in LOAD_PATH) == false
+    push!(LOAD_PATH, ocean_julia_npp);
+end
 
 if (pwd() in LOAD_PATH) == false
     push!(LOAD_PATH, pwd());
