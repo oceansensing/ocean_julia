a5af89ddb719e20d5f1d324304833c87e5b93b37
Donglai Gong <truedichotomy@gmail.com>
2021-09-01 22:29:11 -0400

    Update ocean_dispersion_relation.jl

--- ocean_dispersion_relation.jl
+++ ocean_dispersion_relation.jl
@@ -1,5 +1,5 @@
 L = 1:1000; d = 1:1000;
-Lmatrix = Array{Float64}(undef, length(L), length(d));
+L2d = Array{Float64}(undef, length(L), length(d));
 c(L,d) = (10*L/(2*pi)*tanh(2*pi*d/L))^0.5;
 
 Lmatrix = [fill(convert(Float64,i),(1,length(d))) for i in 1:length(d)]
