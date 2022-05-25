239b862208bbb70df4778e39a6d806c969d61f15
Donglai Gong <truedichotomy@gmail.com>
2022-05-25 15:38:22 -0400

    Update load_era5.jl

--- load_era5.jl
+++ load_era5.jl
@@ -1,11 +1,14 @@
 using Pkg
 
 using PyCall, Glob, NCDatasets, Missings, Interpolations, NaNMath, Dates, Zarr
-using Plots
+using Plots, PlotlySave
 
 cmo = pyimport("cmocean")
 gsw = pyimport("gsw")
 netCDF4 = pyimport("netCDF4")
 plotly()
 
-era5dir = ""
+era5dir = "/Users/gong/oceansensing Dropbox/C2PO/Data/ERA5/"
+yyyy = 2019;
+
+datadir = era5dir * string(yyyy) * "/";
