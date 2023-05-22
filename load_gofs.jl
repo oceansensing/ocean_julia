61bc2a60fea029fc13919b41723eaa95f480514d
Donglai Gong <truedichotomy@gmail.com>
2023-05-22 17:16:00 -0400

    Update load_gofs.jl

--- load_gofs.jl
+++ load_gofs.jl
@@ -4,7 +4,14 @@ using GibbsSeaWater, NetCDF, NCDatasets
 gofs_url = "https://tds.hycom.org/thredds/dodsC/GLBy0.08/expt_93.0";
 gofs = NCDataset(gofs_url, "r");
 
+latmin, latmax = 30.0, 43.0;
+lonmin, lonmax = -82.0, -45.0;
+
+tnow = Dates.datetime2unix(Dates.now());
+t0 = tnow;
+
 lat_gofs = gofs["lat"][:];
 lon_gofs = gofs["lon"][:];
-t_gofs = gofs["time"][:];
+t_gofs = Dates.datetime2unix.(gofs["time"][:]);
 
+(t0min, t0ind) = findmin(abs.(tnow .- t_gofs));
