82b67cca27c87b926e34c559e80d15ff54613acd
Donglai Gong <15986815+truedichotomy@users.noreply.github.com>
2024-09-19 17:16:47 -0400

    added missing2nan function

--- C2PO.jl
+++ C2PO.jl
@@ -1,7 +1,32 @@
 module C2PO
 
 using Dates, Missings
-export gc_distance, rad2deg, deg2rad, histc, meshgrid, nan, findNaNmin, findNaNmax, nanfy, oneDize, datetimemissing2unixtimenan, cdnlp, stresslp
+export missing2nan, gc_distance, rad2deg, deg2rad, histc, meshgrid, nan, findNaNmin, findNaNmax, nanfy, oneDize, datetimemissing2unixtimenan, cdnlp, stresslp
+
+function missing2nan(varin)
+    varin = collect(varin);
+    if (typeof(varin) == Vector{Union{Missing, Int64}}) | (typeof(varin) == Matrix{Union{Missing, Int64}})
+        varout = Array{Float64}(undef,size(collect(varin)));
+        varintypes = typeof.(varin);
+        notmissind = findall(varintypes .!= Missing);
+        missind = findall(varintypes .== Missing); 
+        if isempty(notmissind) != true  
+            varout[notmissind] .= Float64.(varin[notmissind]);
+        end
+        if isempty(missind) != true
+            varout[missind] .= NaN;
+        end
+    elseif (typeof(varin) == Vector{Union{Missing, Float64}}) | (typeof(varin) == Matrix{Union{Missing, Float64}})
+        varout = Float64.(collect(Missings.replace(varin, NaN)));
+    elseif (typeof(varin) == Vector{Missing}) | (typeof(varin) == Matrix{Missing})
+        varout = Array{Float64}(undef,size(collect(varin)));
+        varout .= NaN; 
+    else
+        varout = varin;
+    end
+
+    return varout
+end
 
 function gc_distance(lat1deg::Float64,lon1deg::Float64,lat2deg::Float64,lon2deg::Float64)
     # This code implements Vincenty 1975: https://www.ngs.noaa.gov/PUBS_LIB/inverse.pdf
