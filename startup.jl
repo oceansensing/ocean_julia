5a528899e6954ded70b2dc5f65932a075fabb93b
Donglai Gong <15986815+truedichotomy@users.noreply.github.com>
2024-09-19 17:28:54 -0400

    Update startup.jl

--- startup.jl
+++ startup.jl
@@ -1,15 +1,25 @@
-ENV["PYTHON"] = "/Users/gong/opt/anaconda3/bin/python"
-
-seaexplorer = ENV["HOME"] * "/GitHub/jlglider/seaexplorer"
-if (seaexplorer in LOAD_PATH) == false
-    push!(LOAD_PATH, seaexplorer);
-end
-
-slocum = ENV["HOME"] * "/GitHub/jlglider/slocum"
-if (slocum in LOAD_PATH) == false
-    push!(LOAD_PATH, slocum);
-end
-
-#if (pwd() in LOAD_PATH) == false
-#    push!(LOAD_PATH, pwd());
+#try
+#    using Revise
+#catch e
+#    @warn "Error initializing Revise" exception=(e, catch_backtrace())
 #end
+ENV["PYTHON"] = "/Users/gong/anaconda3/bin/python"
+
+ocean_julia_path = ENV["HOME"] * "/GitHub/jlglider/ocean_julia"
+if (ocean_julia_path in LOAD_PATH) == false
+    push!(LOAD_PATH, ocean_julia_path);
+end
+
+seaexplorer_path = ENV["HOME"] * "/GitHub/jlglider/seaexplorer"
+if (seaexplorer_path in LOAD_PATH) == false
+    push!(LOAD_PATH, seaexplorer_path);
+end
+
+slocum_path = ENV["HOME"] * "/GitHub/jlglider/slocum"
+if (slocum_path in LOAD_PATH) == false
+    push!(LOAD_PATH, slocum_path);
+end
+
+if (pwd() in LOAD_PATH) == false
+    push!(LOAD_PATH, pwd());
+end
